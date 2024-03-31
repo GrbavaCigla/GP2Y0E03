faf37e0686291c2419048d075a82b222a752bbff
Alexa Ognjanovic <alexa.ognjanovic@protonmail.com>
2024-03-31 13:22:24 +0000

    Update example GP2Y0E03.ino

--- GP2Y0E03.ino
+++ GP2Y0E03.ino
@@ -1,14 +1,17 @@
 #include "GP2Y0E03.h";
 
-GP2Y0E03 sensor(A0, true);
+GP2Y0E03 sensorAnalog(A0, true);
+GP2Y0E03 sensorI2C();
 
 void setup() {
   Serial.begin(9600);
 
-  sensor.initialize();
+  sensorI2C.initialize();
 }
 
 void loop() {
-  Serial.println(sensor.read());
+  Serial.print(sensorAnalog.read());
+  Serial.print(" ");
+  Serial.println(sensorI2C.read());
   delay(1000);
 }
